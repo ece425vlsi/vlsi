212ec6b7bb9c1be580a579215bf6a3bcae219c26
townleym <townleym@localhost.localdomain>
2017-01-31 16:39:35 -0500

    creating repo

---
+++ lab02/nand3test.cmd
@@ -0,0 +1,20 @@
+h Vdd
+l Gnd
+analyzer A B C Y
+vector in A B C
+setvector in 000
+s
+setvector in 001
+s
+setvector in 010
+s
+setvector in 011
+s
+setvector in 100
+s
+setvector in 101
+s
+setvector in 110
+s
+setvector in 111
+s
