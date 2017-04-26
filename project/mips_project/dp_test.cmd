d17356a545048daa9c33856ed54a91356068ad0e
nessa <nessa@localhost.localdomain>
2017-04-25 23:19:59 -0400

    Successful exhaustive simulation of shifter

--- project/mips_project/dp_test.cmd
+++ project/mips_project/dp_test.cmd
@@ -12,13 +12,18 @@ vector k k2 k1 k0
 vector result result7 result6 result5 result4 result3 result2 result1 result0
 #vector expected expected7 expected6 expected5 expected4 expected3 expected2 expected1 expected0
 
+############ DEBUG VECTORS ############################
+
+#vector y y7 y6 y5 y4 y3 y2 y1 y0
+#vector sc sc7 sc6 sc5 sc4 sc3 sc2 sc1 sc0
+
 stepsize 250
 
 #analyzer A B Less OP Result y_temp
 analyzer op a b k result zero
 w op a b k result zero
 
-### NOTE: RIGHT IS CONNECTED TO OP3, ARITH IS CONMNECTED TO OP2
+### NOTE: RIGHT IS CONNECTED TO OP3, ARITH IS CONNECTED TO OP2
 
 #F_sll  : alucontrol = 7'b0000011;
 #F_srl  : alucontrol = 7'b0001011;
@@ -55,11 +60,11 @@ proc dec2bin {i {width {}}} {
 
 
 proc cycleInput {} {
-for {set i 0} {$i < 256} {incr i} {
+     for {set i 0} {$i < 256} {incr i} {
 
-    set adec [dec2bin $i 8]
-    #puts "adec is: $adec"
-    setvector a $adec
+     set adec [dec2bin $i 8]
+      #puts "adec is: $adec"
+      setvector a $adec
 
     set j 1
     	      while {$j < 256}  {
@@ -70,7 +75,23 @@ for {set i 0} {$i < 256} {incr i} {
 		  s
 		  checkAnswer
 	      }
+    }
 }
+
+proc cycleInputShift {} {
+
+     for {set i 0} {$i < 256} {incr i} {
+     	 set bdec [dec2bin $i 8]
+	 setvector b $bdec
+
+	 for {set j 0} {$j < 8} {incr j} {
+	     set kdec [dec2bin $j 3]
+	     setvector k $kdec
+	     s
+	     checkAnswer
+	 }
+     }
+
 }
 
 proc checkAnswer {} {
@@ -107,25 +128,26 @@ proc checkAnswer {} {
      } elseif { [query op] == 11 } {
      	set expectedNum [expr [query b] >> [query k]]
      } elseif { [query op] == 15 } {
-     	set expectedNum [expr [query A] >> [query K]] # NEED TO ACCOUNT FOR LOGICAL SHIFT FEATURES
-	if { a7 } { # MUST SET THE BITS SHIFTED IN LOW
-	   set mask 127 #01111111
-	   for {set i 0} {$i < [query K]} {incr i} {
-	       set expectedNum [expr $expectedNum & $mask]
+     	set expectedNum [expr [query b] >> [query k]]
+	if { [query b7] } { # MUST SET THE BITS SHIFTED IN LOW
+	   set mask 128
+	   for {set i 0} {$i < [query k]} {incr i} {
+	       set expectedNum [expr $expectedNum | $mask]
 	       set mask [expr $mask >> 1]
 	   }
+	   puts "mask: $mask"
 	}
      }
 
-     set decex $expectedNum
-     set binex [dec2bin $expectedNum 8]
-     set decre [query result]
-     set binre [dec2bin $decre 8]
+     #set decex $expectedNum
+     #set binex [dec2bin $expectedNum 8]
+     #set decre [query result]
+     #set binre [dec2bin $decre 8]
 
      puts "a is [query a]; b is [query b]"
-     puts "dec expected: $decex"
+     #puts "dec expected: $decex"
      #puts "bin expected: $binex"
-     puts "dec result: $decre"
+     #puts "dec result: $decre"
      #puts "bin result: $binre"
 
      #if { $expectedNum < 240} {
@@ -174,21 +196,16 @@ cycleInput
 #F_srl  : alucontrol = 7'b0001011;
 #F_sra  : alucontrol = 7'b0001111;
 
-setvector k 010
+#setvector k 010
 
 #sll
 setvector op 0000011
-cycleInput
+cycleInputShift
 
 #srl
 setvector op 0001011
-cycleInput
+cycleInputShift
 
 #sra
 setvector op 0001111
-cycleInput
-
-
-
-
-
+cycleInputShift
