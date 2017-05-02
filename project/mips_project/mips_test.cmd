4b678e48f38f95a41bceda2b21753ccbe46967ba
nessa <nessa@localhost.localdomain>
2017-05-02 16:09:19 -0400

    Added changes to mips_test.cmd

--- project/mips_project/mips_test.cmd
+++ project/mips_project/mips_test.cmd
@@ -17,7 +17,27 @@ w ph1 ph2 reset memdata memwrite adr writedata
 
 ################## PROCESSES ############################
 
+#set mem(0) 80020044
+#set mem(1) 80070040
+#set mem(2) 80e30045
+#set mem(3) 00e22025
+#set mem(4) 00642824
+#set mem(5) 00a42820
+#set mem(6) 10a70008
+#set mem(7) 0064302a
+#set mem(8) 10c00001
+#set mem(9) 80050000
+#set mem(10) 00e2302a
+#set mem(11) 00c53820
+#set mem(12) 00e23822
+#set mem(13) 0800000f
+#set mem(14) 80070000
+#set mem(15) a0470047
+#set mem(16) 00000003
+#set mem(17) 00000005
+#set mem(18) 0000000c
 
+#array set mem {0 80020044}
 
 proc dec2bin {i {width {}}} {
     #returns the binary representation of $i
@@ -44,32 +64,89 @@ proc dec2bin {i {width {}}} {
     return $sign$res
 }
 
+
+proc mem_read { loc } {
+
+     if { $loc == 0} {
+     	return 0x80020044
+     } elseif { $loc == 1 } {
+       return 0x80070040
+     } elseif { $loc == 2 } {
+       return 0x80e30045
+     } elseif { $loc == 3 } {
+       return 0x00e22025
+     } elseif { $loc == 4 } {
+       return 0x00642824
+     } elseif { $loc == 5 } {
+       return 0x00a42820
+     } elseif { $loc == 6 } {
+       return 0x10a70008
+     } elseif { $loc == 7 } {
+       return 0x0064302a
+     } elseif { $loc == 8 } {
+       return 0x10c00001
+     } elseif { $loc == 9 } {
+       return 0x80050000
+     } elseif { $loc == 10 } {
+       return 0x00e2302a
+     } elseif { $loc == 11 } {
+       return 0x00c53820
+     } elseif { $loc == 12 } {
+       return 0x00e23822
+     } elseif { $loc == 13 } {
+       return 0x0800000f
+     } elseif { $loc == 14 } {
+       return 0x80070000
+     } elseif { $loc == 15 } {
+       return 0xa0470047
+     } elseif { $loc == 16 } {
+       return 0x00000003
+     } elseif { $loc == 17 } {
+       return 0x00000005
+     } elseif { $loc == 18 } {
+       return 0x0000000c
+     } else {
+       return 0
+     }
+
+}
+
 proc mem_acc {} {
 
      set addr [query adr]
      set wordaddr [expr $addr >> 2]
-     set hexword mem($wordaddr)
-     scan $hexword %x word
+
+     puts "addr: $addr"
+     puts "wordaddr: $wordaddr"
+
+     #set hexword $mem($wordaddr)
+     #scan $hexword %x word
+     set hexword [mem_read $wordaddr]
+     puts "instr should be: $hexword"
+     #set word [expr 0x$hexword]
+     set word $hexword
+     puts "word on input should be: $word"
      if { [query adr1] && [query adr0] } {
      	set membyte [expr $word >> 24]
-	set membyte [expr $membyte & 15]
+	set membyte [expr $membyte & 0xff]
 	set binbyte [dec2bin $membyte 8]
 	setvector memdata $binbyte
 	#return $membyte
      } elseif { [query adr1] && ~[query adr0] } {
         set membyte [expr $word >> 16]
-	set membyte [expr $membyte & 15]
+	set membyte [expr $membyte & 0xff]
 	set binbyte [dec2bin $membyte 8]
 	setvector memdata $binbyte
 	#return $membyte
-     } elseif { [query adr1] && ~[query adr0] } {
+     } elseif { ~[query adr1] && [query adr0] } {
         set membyte [expr $word >> 8]
-	set membyte [expr $membyte & 15]
+	set membyte [expr $membyte & 0xff]
 	set binbyte [dec2bin $membyte 8]
 	setvector memdata $binbyte
 	#return $membyte
      } else {
-	set membyte [expr $membyte & 15]
+       	set membyte $word
+	set membyte [expr $membyte & 0xff]
 	set binbyte [dec2bin $membyte 8]
 	setvector memdata $binbyte
 	#return $membyte
@@ -91,60 +168,61 @@ proc run_mips {} {
      # ACCESS MEMORY
      mem_acc
 
+     set addrout [query adr]
+     set memout [query memdata]
+     puts "adr: $addrout"
+     puts "memdata: $memout"
+
      s
 
 }
 
 ####################### EXECUTED CODE: ########################################
 
-set mem(0) 80020044
-set mem(1) 80070040
-set mem(2) 80e30045
-set mem(3) 00e22025
-set mem(4) 00642824
-set mem(5) 00a42820
-set mem(6) 10a70008
-set mem(7) 0064302a
-set mem(8) 10c00001
-set mem(9) 80050000
-set mem(10) 00e2302a
-set mem(11) 00c53820
-set mem(12) 00e23822
-set mem(13) 0800000f
-set mem(14) 80070000
-set mem(15) a0470047
-set mem(16) 00000003
-set mem(17) 00000005
-set mem(18) 0000000c
+
 
 h reset
 
 l ph1
 h ph2
 setvector memdata 00000000
+s
 
 # RUN THE CLOCK A FEW TIMES WITH RESET HIGH
 
 h ph1
 l ph2
+s
 
 l ph1
 h ph2
+s
 
 h ph1
 l ph2
+s
 
 l ph1
 h ph2
+s
 
 l reset
+
+#h ph1
+#l ph2
+
+#s
 
 # NOW LET'S START RUNNING MIPS:
 
 run_mips
 
-while { ~[query memwrite] } {
-      run_mips
+#while { ~[query memwrite] } {
+#      run_mips
+#}
+
+for {set i 0} {$i < 15} {incr i} {
+    run_mips
 }
 
 assert adr [dec2bin 76 8]
